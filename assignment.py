c6dd40b0e18b1e295303f76c9dfe995761dd4a39
Nikhil14352 <142996314+Nikhil14352@users.noreply.github.com>
2023-08-24 09:43:36 +0530

    i'm printing membership operator

---
+++ assignment.py
@@ -0,0 +1,13 @@
+#Membership operator in and not in
+str="nikhil"
+str1="neeraj"
+str2="harshith"
+str3="nithin"
+print(str in str3)
+print(str2 in str)
+print(str3 not in str2)
+
+
+s="1 2 4"
+s1="2 3 5"
+print(s not in s1)
